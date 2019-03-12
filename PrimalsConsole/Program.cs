d52ab960b776fd6ba74ddba413afe5a23e4fe28d
pleszczu1 <1piotr.leszczynski@gmail.com>
2019-03-12 22:46:18 +0100

    Dodaj pliki projektów.

---
+++ PrimalsConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
